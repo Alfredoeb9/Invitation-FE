47b55990ac4c8afa6e27f39b7d9e7fa37a020c81
Alfredoeb9 <alfredoeb96@gmail.com>
2024-06-26 14:46:03 -0700

    [canvas] created canvas layer and created color background tool to change background of canvas

--- src/components/InvitationBuilder.tsx
+++ src/components/InvitationBuilder.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { LayoutPanelLeft } from "lucide-react";
 import { Shapes } from "lucide-react";
 import { CloudUpload } from "lucide-react";
@@ -11,9 +11,42 @@ import BackgroundTool from "./tools/BackgroundTool";
 import TextTool from "./tools/TextTool";
 import ShapeTool from "./tools/ShapeTool";
 import UploadTool from "./tools/UploadTool";
+import CreateComponent from "./CreateComponent";
+
+interface InfoTypes {
+  name: string;
+  type: string;
+  id: number;
+  height: number;
+  width: number;
+  z_index: number;
+  color: string;
+  image: string;
+  setCurrentComponent: Dispatch<SetStateAction<string>>;
+}
+
+export interface ComponentsTypes {
+  info: InfoTypes[];
+}
 
 export default function InvitationBuilder() {
-  const [state, setState] = useState("");
+  const [state, setState] = useState<HTMLElement | any | undefined>(undefined);
+  const [currentComponent, setCurrentComponent] = useState<any>(undefined);
+  const [color, setColor] = useState<string>("");
+  const [components, setComponents] = useState<InfoTypes[]>([
+    {
+      name: "main_frame",
+      type: "rect",
+      id: Math.floor(Math.random() * 100 + 1),
+      height: state?.height || 450,
+      width: state?.width || 650,
+      z_index: 1,
+      color: "#fff",
+      image: "",
+      setCurrentComponent: (a: React.SetStateAction<string>) =>
+        setCurrentComponent(a),
+    },
+  ]);
   const [show, setShow] = useState({ status: true, name: "" });
   const invitationTools = [
     { id: 0, icon: <LayoutPanelLeft />, text: "Design" },
@@ -32,8 +65,36 @@ export default function InvitationBuilder() {
     });
   };
 
+  const moveElement = () => {
+    console.log("move");
+  };
+
+  const resizeElement = () => {
+    console.log("resize");
+  };
+
+  const rotateElement = () => {
+    console.log("rotateElement");
+  };
+
+  const removeComponent = () => {
+    const com = components.find((c) => c.id === currentComponent.id);
+    const temp = components.filter((c) => c.id !== currentComponent.id);
+
+    // com.image = "";
+    // setImage("");
+    // setComponents([...temp, com]);
+  };
+
+  useEffect(() => {
+    if (currentComponent) {
+      const index = components.findIndex((c) => c.id === currentComponent.id);
+      components[index].color = color || currentComponent.color;
+    }
+  }, [color]);
+
   return (
-    <div className="">
+    <div>
       <div className="flex h-full w-screen">
         <div className="w-[80px] bg-slate-900 z-50 h-full text-gray-400 overflow-y-auto">
           {invitationTools.map((tool) => (
@@ -42,9 +103,15 @@ export default function InvitationBuilder() {
               className={` ${
                 show.name === tool.text.toLowerCase() ? "bg-[#252627]" : ""
               } w-full h-[80px] cursor-pointer flex justify-center flex-col items-center gap-1 hover:text-gray-100`}
-              onClick={() =>
-                setElements(tool.text.toLowerCase(), tool.text.toLowerCase())
-              }
+              onClick={() => {
+                // console.log("running");
+
+                setElements(tool.text.toLowerCase(), tool.text.toLowerCase());
+
+                // if (!show.status) {
+                //   setShow({ name: "", status: true });
+                // }
+              }}
             >
               <span className="text-2xl">{tool.icon}</span>
               <span className="text-xs font-medium">{tool.text}</span>
@@ -52,7 +119,7 @@ export default function InvitationBuilder() {
           ))}
         </div>
 
-        <div className="h-full w-[calc(100% - 75px)]">
+        <div className="h-full w-[calc(100%-75px)]">
           <div
             className={`${
               show.status ? "p-0 -left-[350px]" : "px-8 left-[75px] py-5"
@@ -65,7 +132,7 @@ export default function InvitationBuilder() {
               <ArrowLeft />
             </div>
 
-            {state.length > 0 && (
+            {state?.length > 0 && (
               <>
                 <h3>
                   {show.name.charAt(0).toUpperCase() + show.name.slice(1)}
@@ -81,6 +148,60 @@ export default function InvitationBuilder() {
 
                 {show.name.toLowerCase() === "upload" && <UploadTool />}
               </>
+            )}
+          </div>
+
+          <div className="w-full h-full">
+            <div
+              className={`flex justify-center relative items-center h-full ${
+                !currentComponent
+                  ? "w-full"
+                  : "w-[calc(100%-250px)] overflow-hidden"
+              }`}
+            >
+              <div className="m-w-[650px] m-h-[480px] flex justify-center items-center overflow-hidden">
+                <div
+                  id="main_design"
+                  className="w-auto relative h-auto overflow-hidden"
+                >
+                  {components.map((comp, i) => (
+                    <CreateComponent
+                      key={i}
+                      info={comp}
+                      currentComponent={currentComponent}
+                      removeComponent={removeComponent}
+                    />
+                  ))}
+                </div>
+              </div>
+            </div>
+
+            {currentComponent && (
+              <div className="h-[calc(100%-46px)] w-[250px] text-gray-300 bg-[#252627] px-3 py-2 absolute right-0 top-[46px]">
+                <div className="flex gap-4 flex-col items-start h-full px-3 justify-start">
+                  <div className="flex gap-4 justify-start items-start">
+                    <span>Color: </span>
+                    <label
+                      className="w-[30px] h-[30px] cursor-pointer rounded-md"
+                      htmlFor="color"
+                      style={{
+                        background: `${
+                          currentComponent?.color &&
+                          currentComponent.color !== "#fff"
+                            ? currentComponent.color
+                            : "gray"
+                        }`,
+                      }}
+                    ></label>
+                    <input
+                      onChange={(e) => setColor(e.currentTarget.value)}
+                      type="color"
+                      className="invisible"
+                      id="color"
+                    />
+                  </div>
+                </div>
+              </div>
             )}
           </div>
         </div>
